acdbbd82ee907fa033bb0c4526e8e659db167392
arraxy-frost <artyom.prilip@gmail.com>
2025-09-17 12:29:48 +0600

    feat(card): update table layout, add text warning block

--- src/App.vue
+++ src/App.vue
@@ -19,54 +19,95 @@ const onClickDelete = (id: any) => {
 </script>
 
 <template>
-    <header>
-        <span class="header-title">Учетные записи</span>
-        <el-button type="default" size="large" circle style="border-radius: 0" @click="onClickAdd">
-            <el-icon>
-                <Plus />
-            </el-icon>
-        </el-button>
-    </header>
-    <el-table :data="users" style="width: 100%; padding: 16px; border-radius: 16px">
-        <el-table-column label="Метка" width="160">
-            <template #default="scope">
-                <el-input v-model="scope.row.tags" size="large" maxlength="50" />
-            </template>
-        </el-table-column>
-        <el-table-column align="center" label="Тип" width="110">
-            <template #default="scope">
-                <el-select v-model="scope.row.type" size="large">
-                    <el-option v-for="t in UserCardType" :key="t" :label="t" :value="t"></el-option>
-                </el-select>
-            </template>
-        </el-table-column>
-        <el-table-column prop="login" align="center" label="Логин" width="180">
-            <template #default="scope">
-                <el-input v-model="scope.row.login" size="large" maxlength="100" />
-            </template>
-        </el-table-column>
-        <el-table-column label="Пароль" align="center" width="300">
-            <template #default="scope">
-                <el-input
-                    v-model="scope.row.password"
+    <el-container>
+        <el-header style="display: flex; flex-direction: column; gap: 16px; height: fit-content; padding-bottom: 0;">
+            <div
+                style="
+                    display: flex;
+                    flex-direction: row;
+                    gap: 8px;
+                    width: 100%;
+                    align-items: center;
+                "
+            >
+                <span class="header-title">Учетные записи</span>
+                <el-button
+                    type="default"
                     size="large"
-                    type="password"
-                    show-password
-                    clearable
-                    maxlength="100"
-                />
-            </template>
-        </el-table-column>
-        <el-table-column align="left" width="100">
-            <template #default="scope">
-                <el-button size="large" @click="onClickDelete(scope.row)" style="border: none">
+                    circle
+                    style="border-radius: 0"
+                    @click="onClickAdd"
+                >
                     <el-icon>
-                        <Delete />
+                        <Plus />
                     </el-icon>
                 </el-button>
-            </template>
-        </el-table-column>
-    </el-table>
+            </div>
+            <div style="background: aliceblue; padding: 8px; font-size: 13px; width: 100%">
+                Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;
+            </div>
+        </el-header>
+        <el-main style="padding-top: 8px;">
+            <el-table :data="users" >
+                <el-table-column label="Метка">
+                    <template #default="scope">
+                        <el-input v-model="scope.row.tags" size="large" maxlength="50" />
+                    </template>
+                </el-table-column>
+                <el-table-column align="center" label="Тип" width="140">
+                    <template #default="scope">
+                        <el-select v-model="scope.row.type" size="large">
+                            <el-option
+                                v-for="t in UserCardType"
+                                :key="t"
+                                :label="t"
+                                :value="t"
+                            ></el-option>
+                        </el-select>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="login" align="center" label="Логин">
+                    <template #default="scope">
+                        <el-input v-model="scope.row.login" size="large" maxlength="100" />
+                    </template>
+                </el-table-column>
+                <el-table-column label="Пароль" align="center">
+                    <template #default="scope">
+                        <el-input
+                            v-model="scope.row.password"
+                            size="large"
+                            type="password"
+                            show-password
+                            clearable
+                            maxlength="100"
+                        />
+                    </template>
+                </el-table-column>
+                <el-table-column align="center" width="80">
+                    <template #default="scope">
+                        <el-button
+                            size="large"
+                            @click="onClickDelete(scope.row)"
+                            style="
+                                border: none;
+                                width: 48px;
+                                height: 48px;
+                                min-width: 48px;
+                                padding: 0;
+                                display: flex;
+                                align-items: center;
+                                justify-content: center;
+                            "
+                        >
+                            <el-icon>
+                                <Delete />
+                            </el-icon>
+                        </el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </el-main>
+    </el-container>
 </template>
 
 <style scoped>
